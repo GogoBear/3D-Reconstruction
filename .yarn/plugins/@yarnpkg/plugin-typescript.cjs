dba9d2d6a6762926e50507f73906ec87d76600ed
GogoBear <102480361+GogoBear@users.noreply.github.com>
2020-05-15 19:18:32 +0000

    First version!

--- .yarn/plugins/@yarnpkg/plugin-typescript.cjs
+++ .yarn/plugins/@yarnpkg/plugin-typescript.cjs
@@ -3,4 +3,4 @@
 module.exports = {
 name: "@yarnpkg/plugin-typescript",
 factory: function (require) {
-var plugin=(()=>{var Ft=Object.create,H=Object.defineProperty,Bt=Object.defineProperties,Kt=Object.getOwnPropertyDescriptor,zt=Object.getOwnPropertyDescriptors,Gt=Object.getOwnPropertyNames,Q=Object.getOwnPropertySymbols,$t=Object.getPrototypeOf,ne=Object.prototype
+var plugin=(()=>{var Ft=Object.create,H=Object.defineProperty,Bt=Object.defineProperties,Kt=Object.getOwnPropertyDescriptor,zt=Object.getOwnPropertyDescriptors,Gt=Object.getOwnPropertyNames,Q=Object.getOwnPropertySymbols,$t=Object.getPrototypeOf,ne=Object.prototype.hasOwnProperty,De=Object.prototype.propertyI
