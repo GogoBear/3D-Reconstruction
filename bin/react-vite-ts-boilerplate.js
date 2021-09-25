bf4e0021e5912624fa67f2dd63307b4c63ffeef7
GogoBear <102480361+GogoBear@users.noreply.github.com>
2021-09-25 09:48:52 +0000

    version 0.3.6

--- bin/react-vite-ts-boilerplate.js
+++ bin/react-vite-ts-boilerplate.js
@@ -1,4 +1,7 @@
 #! /usr/bin/env node
 const { execSync } = require('child_process');
 const path = require('path');
-const fs = 
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.
