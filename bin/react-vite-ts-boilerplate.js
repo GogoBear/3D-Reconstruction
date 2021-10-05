1b6dd4447f48208bd6bfdc13ac1bcabb8aba587e
GogoBear <102480361+GogoBear@users.noreply.github.com>
2021-10-05 05:57:28 +0000

    Add build status

--- bin/react-vite-ts-boilerplate.js
+++ bin/react-vite-ts-boilerplate.js
@@ -13,4 +13,4 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = 'https://gith
+const GIT_REPO = 'https://github.com/choisohyun/react-vite-ts-boilerplate.git';
