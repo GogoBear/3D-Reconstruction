62b02b13f53045d2abfc5b2222ce652467dfb103
GogoBear <102480361+GogoBear@users.noreply.github.com>
2021-12-26 06:13:46 +0000

    minor cleanup

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+
+import App from '@/App';
+import '@/assets/scss/style.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+root.render(app);
