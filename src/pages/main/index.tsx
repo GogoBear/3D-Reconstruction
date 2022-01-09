0c873f6e91a2514d2d51b1f1e7652160d44bc5c4
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-01-09 07:31:26 +0000

    Fix security vulnerabilities

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -5,4 +5,9 @@ const Main = () => {
   const { count, increment } = useCounter();
 
   const env: 'development' | 'production' = useMemo(() => {
-    return import.meta.env.VITE_TEST || process.env.NODE_ENV === 'test' ? 'development' : 'production'
+    return import.meta.env.VITE_TEST || process.env.NODE_ENV === 'test' ? 'development' : 'production';
+  }, []);
+
+  return (
+    <>
+      <h2 className="test"
