1359f5501c9d9587f4e313ef6cf854250674b842
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-01-16 08:51:55 +0000

    adding project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,4 +2,7 @@ import React from 'react';
 import { RouteObject } from 'react-router-dom';
 import Layout from '@/pages/layout';
 import Main from '@/pages/main';
-import NotF
+import NotFound from '@/pages/NotFound';
+
+export default [
+  
