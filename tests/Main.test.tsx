a0b7a187b71fea22ff111e4a6e260d54514df4e3
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-02-20 10:14:21 +0000

    expression

--- tests/Main.test.tsx
+++ tests/Main.test.tsx
@@ -5,4 +5,7 @@ import Main from '../src/pages/main';
 describe('Simple working test', () => {
   it('the title is visible', () => {
     render(<Main />);
-    expect(screen.getByText(/Hello Vite 
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeCalled();
+  });
+
+  it('should incr
