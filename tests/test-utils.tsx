9d2db67075985faf088e718e61fda91161be19e8
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-03-06 11:45:13 +0000

    Rename readme

--- tests/test-utils.tsx
+++ tests/test-utils.tsx
@@ -1,4 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
-const customRender = (ui: React.ReactElem
+const customRender = (ui: React.ReactElement, options = {}) =>
+  render(ui, {
+    // wrap pr
