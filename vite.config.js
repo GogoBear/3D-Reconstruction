4fe131efa982f1c6695e31f0fd7626c4a8b16616
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-03-20 17:57:39 +0000

    gitignore edit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { createHt
+import { createHtmlPlugin } from 'vite-plugin-html';
+import che
