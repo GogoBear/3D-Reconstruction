960b8749be9cca3123f47b8456e5f7466839faec
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-03-20 21:48:59 +0000

    Moar docs

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import che
+import checker from 'vite-plugin-checker';
+import path from 'path';
+
+import
