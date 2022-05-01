6d712d25ec36dabdc9297f04d5de1d009797cf45
GogoBear <102480361+GogoBear@users.noreply.github.com>
2022-05-01 10:02:33 +0000

    Prepare next development iteration.

--- vite.config.js
+++ vite.config.js
@@ -44,4 +44,6 @@ export default defineConfig(({ mode }) => {
         parser: ctx.parser ? 'sugarss' : false,
         map: ctx.env === 'development' ? ctx.map : false,
         plugins: {
-          'p
+          'postcss-import': {},
+          'postcss-nested': {},
+        
